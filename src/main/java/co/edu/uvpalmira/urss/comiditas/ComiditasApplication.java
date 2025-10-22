5d533713d006621ea0350fe4cc945a97faace718
Juan Pablo Ramirez Caracas <ramirezcaracasj@gmail.com>
2025-10-22 16:03:53 -0500

    initial commit

---
+++ src/main/java/co/edu/uvpalmira/urss/comiditas/ComiditasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uvpalmira.urss.comiditas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComiditasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComiditasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uvpalmira/urss/comiditas/ComiditasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uvpalmira.urss.comiditas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComiditasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
