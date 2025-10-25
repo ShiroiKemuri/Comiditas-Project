ca27d345f276d66e64eb11dc635b6ac50d69ec44
Juan Pablo Ramirez Caracas <ramirezcaracasj@gmail.com>
2025-10-24 19:58:01 -0500

    More Funtional Corrections + add AuthController

--- src/main/java/co/edu/uvpalmira/urss/Backend/modelo/Administrador.java
+++ src/main/java/co/edu/uvpalmira/urss/Backend/modelo/Administrador.java
@@ -1,26 +1,25 @@
 package co.edu.uvpalmira.urss.Backend.modelo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
-public class Administrador extends Persona{
+public class Administrador {
     
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    private Long id;
     private String usuario;   
     private String contrasena;
-    
-    public Administrador(String nombre, int edad, int identificacion, String genero, String usuario, String contrasena) {
-        super(nombre, edad, identificacion, genero);
-        this.usuario = usuario;
-        this.contrasena = contrasena;
-    }
+    private String nombre;
 
-    public String getContraseña() {
-        return contrasena;
-    }
 }
