0a686be02f65cae53380e3a0f1c1641eec28339f
Juan Pablo Ramirez Caracas <ramirezcaracasj@gmail.com>
2025-10-24 21:56:16 -0500

    just for nothing ig

--- src/main/java/co/edu/uvpalmira/urss/Backend/modelo/IRepository/AdministradorRepo.java
+++ src/main/java/co/edu/uvpalmira/urss/Backend/modelo/IRepository/AdministradorRepo.java
@@ -1,12 +1,12 @@
 package co.edu.uvpalmira.urss.Backend.modelo.IRepository;
 
-import java.util.List;
+//import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import co.edu.uvpalmira.urss.Backend.modelo.Administrador;
 
-public interface AdministradorRepo  extends JpaRepository<Administrador, Long>{
+public interface AdministradorRepo extends JpaRepository<Administrador, Long> {
 
     Optional<Administrador> findByUsuario(String usuario);
 
