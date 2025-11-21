97f8114a2796631edb2b730036af54ad8e093196
Borja <jhoan.daner2005@gmail.com>
2025-11-20 19:10:58 -0500

    n

--- src/main/java/co/edu/uvpalmira/urss/Backend/Controller/CategoryController.java
+++ src/main/java/co/edu/uvpalmira/urss/Backend/Controller/CategoryController.java
@@ -1,7 +1,7 @@
 package co.edu.uvpalmira.urss.Backend.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +30,7 @@ public class CategoryController {
         return categoryService.getCategoryById(id);
     }
 
-    @PutMapping("/deleteCategory/{id}")
+    @DeleteMapping("/deleteCategory/{id}")
     public void deleteCategory(@PathVariable("id") Long id) {
         categoryService.deleteCategory(id);
     }
