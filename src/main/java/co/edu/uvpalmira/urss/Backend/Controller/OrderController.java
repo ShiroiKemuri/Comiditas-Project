4a30e63b626fff8f9dddc1164c20bde9ceac9541
Borja <jhoan.daner2005@gmail.com>
2025-11-24 18:10:47 -0500

    Otros cambios

--- src/main/java/co/edu/uvpalmira/urss/Backend/Controller/OrderController.java
+++ src/main/java/co/edu/uvpalmira/urss/Backend/Controller/OrderController.java
@@ -1,10 +1,12 @@
 package co.edu.uvpalmira.urss.Backend.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import co.edu.uvpalmira.urss.Backend.DTO.OrderDTO;
 import co.edu.uvpalmira.urss.Backend.BusinessLogic.OrderService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class OrderController {
     private OrderService orderService;
 
     @GetMapping("/today")
-    // En un futuro, con Spring Security, añadirías: @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<OrderDTO>> getTodaysOrders() {
         return ResponseEntity.ok(orderService.getTodaysOrders());
     }
--- src/main/java/co/edu/uvpalmira/urss/Backend/IRepository/OrderRepo.java
+++ src/main/java/co/edu/uvpalmira/urss/Backend/IRepository/OrderRepo.java
@@ -1,12 +1,14 @@
 package co.edu.uvpalmira.urss.Backend.IRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import co.edu.uvpalmira.urss.Backend.Model.Order;
-//import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface OrderRepo extends JpaRepository<Order, Long> {
         List<Order> findAllByCreationTimestampBetweenOrderByCreationTimestampDesc(LocalDateTime start, LocalDateTime end);
 }
