d55658191b3b0bb241758be053b026af1a31cbc7
Shiroi <ramirezcaracasj@gmail.com>
2025-11-26 19:44:17 -0500

    Refactor ComiditasApplication and update Sells model to include product relationship

--- src/main/java/co/edu/uvpalmira/urss/Backend/ComiditasApplication.java
+++ src/main/java/co/edu/uvpalmira/urss/Backend/ComiditasApplication.java
@@ -1,10 +1,12 @@
 package co.edu.uvpalmira.urss.Backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EntityScan(basePackages = { "co.edu.uvpalmira.urss.Backend.Model" })
 public class ComiditasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/co/edu/uvpalmira/urss/Backend/Model/Sells.java
+++ src/main/java/co/edu/uvpalmira/urss/Backend/Model/Sells.java
@@ -1,7 +1,9 @@
 package co.edu.uvpalmira.urss.Backend.Model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -32,7 +36,8 @@ public class Sells {
     @Column(name = "Total_Amount")
     private double totalAmount;
 
-    @Column(name = "Product")
+    @ManyToOne
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private Producto products;
 
     @Column(name = "Quantity")
